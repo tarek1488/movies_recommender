1f3e4fe42ccbd727ea91d7fa3ca952330c4cbdaa
Tarek Ashraf <tarekshalaby2015@gmail.com>
2024-11-22 16:49:03 +0200

    code cleaning

--- recommender.py
+++ recommender.py
@@ -15,28 +15,6 @@ movies_vectors = get_sentense_vector(train_data, model, VECTOR_SIZE)
 joblib.dump(movies_vectors, 'movies_vectors.pkl')
 model.save('train_model.model')
 
-# def recommender(movie_id):
-#     # Check if the movie exists
-#     movie = meta_data.loc[meta_data['show_id'] == movie_id]
-#     if movie.empty:
-#         print("Movie not found")
-#         return
-
-#     # Process the movie
-#     processed_movie = process_data(movie.iloc[0])
-#     tokenized_movie = processed_movie.split()
-#     movie_vector = get_average_words(tokenized_movie, model, vector_size, model.wv.key_to_index)
-
-#     # Calculate similarity scores
-#     similarity_scores = cosine_similarity([movie_vector], sentense_vectors)
-#     similar_movies = list(enumerate(similarity_scores[0]))
-#     sorted_similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)[1:11]
-
-#     # Display the top 10 similar movies
-#     print("Top 10 similar movies:")
-#     recommended_movies = df.iloc[[i for i, _ in sorted_similar_movies]]
-#     return recommended_movies[['title', 'description', 'type']]
-
 
 
 
